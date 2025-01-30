5170a656cc8e56aa192fb53f7a66ce178a6fbab7
ivailoGeorgiev <138701075+ivailo654@users.noreply.github.com>
2025-01-30 23:29:42 +0200

    commit

--- bootup_GPIOD_leds/Core/Src/main.c
+++ bootup_GPIOD_leds/Core/Src/main.c
@@ -150,7 +150,17 @@ static void MX_GPIO_Init(void)
 }
 void write_pin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState)
 {
+	 assert_param(IS_GPIO_PIN(GPIO_Pin));
+	 assert_param(IS_GPIO_PIN_ACTION(PinState));
 
+	 if(PinState != GPIO_PIN_RESET)
+	  {
+	    GPIOx->BSRR = GPIO_Pin;
+	  }
+	  else
+	  {
+	    GPIOx->BSRR = (uint32_t)GPIO_Pin << 16U;
+	  }
 }
 /* USER CODE END 4 */
 
